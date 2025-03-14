4e2c899dc2fc8d250947a122fb3da8ccca19b4ca
sergei p <s1mple40rm@gmail.com>
2025-03-14 16:22:50 +0300

    remove cancel

--- pkg/gopool/go_pool.go
+++ pkg/gopool/go_pool.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"runtime"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/panjf2000/ants"
@@ -13,20 +12,11 @@ import (
 
 const noexpiry = 72 * time.Hour
 
-var (
-	ErrNotStarted      = errors.New("not started")
-	ErrAlreadyStarted  = errors.New("already started")
-	ErrAlreadyFinished = errors.New("already finished")
-)
-
 type GoPool struct {
-	wg                *sync.WaitGroup
-	errSync           *sync.Mutex
-	err               error
-	pool              *ants.Pool
-	cancel            func()
-	cancelOnce        *sync.Once
-	started, finished atomic.Bool
+	wg      *sync.WaitGroup
+	errSync *sync.Mutex
+	err     error
+	pool    *ants.Pool
 }
 
 func New() (*GoPool, error) {
@@ -36,25 +26,13 @@ func New() (*GoPool, error) {
 	}
 
 	return &GoPool{
-		wg:         &sync.WaitGroup{},
-		errSync:    &sync.Mutex{},
-		pool:       pool,
-		cancelOnce: &sync.Once{},
+		wg:      &sync.WaitGroup{},
+		errSync: &sync.Mutex{},
+		pool:    pool,
 	}, nil
 }
 
 func (p *GoPool) Run(ctx context.Context, f func(ctx context.Context) error) error {
-	if p.finished.Load() {
-		return ErrAlreadyFinished
-	}
-
-	if p.started.Load() {
-		return ErrAlreadyStarted
-	}
-
-	p.started.Store(true)
-
-	ctx, p.cancel = context.WithCancel(ctx)
 	p.wg.Add(1)
 
 	if err := p.pool.Submit(func() {
@@ -78,32 +56,9 @@ func (p *GoPool) Run(ctx context.Context, f func(ctx context.Context) error) err
 	return nil
 }
 
-func (p *GoPool) Cancel() {
-	if p.started.Load() {
-		p.cancelOnce.Do(func() {
-			p.cancel()
-			p.cancel = nil
-		})
-	}
-}
-
-func (p *GoPool) WaitFinish() error {
-	if p.finished.Load() {
-		return ErrAlreadyFinished
-	}
-
-	if !p.started.Load() {
-		return ErrNotStarted
-	}
-
+func (p *GoPool) Wait() error {
 	p.wg.Wait()
 	p.pool.Release()
-	p.finished.Store(true)
-
-	if p.cancel != nil {
-		p.cancel()
-		p.cancel = nil
-	}
 
 	return p.err
 }
--- pkg/gopool/go_pool_test.go
+++ pkg/gopool/go_pool_test.go
@@ -2,6 +2,7 @@ package gopool
 
 import (
 	"context"
+	"errors"
 	"testing"
 	"time"
 
@@ -22,13 +23,15 @@ func TestGoPool(t *testing.T) {
 		})
 		require.NoError(t, err)
 
-		err = p.WaitFinish()
+		err = p.Wait()
 		require.NoError(t, err)
 	})
 
-	t.Run("run error started", func(t *testing.T) {
+	t.Run("error", func(t *testing.T) {
 		p, err := New()
 		require.NoError(t, err)
+
+		tmpErr := errors.New("error")
 
 		err = p.Run(ctx, func(ctx context.Context) error {
 			time.Sleep(time.Second)
@@ -38,69 +41,11 @@ func TestGoPool(t *testing.T) {
 		require.NoError(t, err)
 
 		err = p.Run(ctx, func(ctx context.Context) error {
-			return nil
-		})
-		require.ErrorIs(t, err, ErrAlreadyStarted)
-
-		err = p.WaitFinish()
-		require.NoError(t, err)
-	})
-
-	t.Run("run error finished", func(t *testing.T) {
-		p, err := New()
-		require.NoError(t, err)
-
-		err = p.Run(ctx, func(ctx context.Context) error {
-			return nil
+			return tmpErr
 		})
 		require.NoError(t, err)
 
-		err = p.WaitFinish()
-		require.NoError(t, err)
-
-		err = p.Run(ctx, func(ctx context.Context) error {
-			return nil
-		})
-		require.ErrorIs(t, err, ErrAlreadyFinished)
-	})
-
-	t.Run("finish error finished", func(t *testing.T) {
-		p, err := New()
-		require.NoError(t, err)
-
-		err = p.Run(ctx, func(ctx context.Context) error {
-			return nil
-		})
-		require.NoError(t, err)
-
-		err = p.WaitFinish()
-		require.NoError(t, err)
-
-		err = p.WaitFinish()
-		require.ErrorIs(t, err, ErrAlreadyFinished)
-	})
-
-	t.Run("cancel", func(t *testing.T) {
-		p, err := New()
-		require.NoError(t, err)
-
-		err = p.Run(ctx, func(ctx context.Context) error {
-			t := time.NewTimer(30 * time.Second)
-
-			for {
-				select {
-				case <-t.C:
-					return nil
-				case <-ctx.Done():
-					return ctx.Err()
-				}
-			}
-		})
-		require.NoError(t, err)
-
-		p.Cancel()
-
-		err = p.WaitFinish()
-		require.ErrorIs(t, err, context.Canceled)
+		err = p.Wait()
+		require.ErrorIs(t, err, tmpErr)
 	})
 }
